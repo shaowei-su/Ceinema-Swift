87d6c5673e3342526834e2467f1cf26df9c36450
shaowei-su <shaow.su@gmail.com>
2015-10-21 11:09:11 -0400

    Change the reachability test.

--- CeinemaNew/Reachability.swift
+++ CeinemaNew/Reachability.swift
@@ -16,7 +16,7 @@ public class Reachability {
     class func isConnectedToNetwork() -> Bool { 
         
         var Status:Bool = false
-        let url = NSURL(string: "http://google.com/")
+        let url = NSURL(string: "http://ceitraining.com/")
         let request = NSMutableURLRequest(URL: url!)
         request.HTTPMethod = "HEAD"
         request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
